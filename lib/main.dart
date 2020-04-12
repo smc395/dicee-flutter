de953b2f2674e693b59e1c71e42ab8ee45df58e9
Sung Yan Micah Chao <symchao@gmail.com>
2020-04-11 20:34:54 -1000

    Code for the dicee flutter project. Added text above the dice to print out the dice total.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,72 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
+      home: SafeArea(
+        child: Scaffold(
           backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Text('Dicee'),
+            backgroundColor: Colors.red,
+          ),
+          body: DicePage(),
         ),
-        body: DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+  int total = 2;
+
+  void getDice() {
+    setState(() {
+      leftDiceNum = Random().nextInt(6) + 1;
+      rightDiceNum = Random().nextInt(6) + 1;
+      total = leftDiceNum + rightDiceNum;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text(
+          '$total',
+          style: TextStyle(
+              fontSize: 40.0, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  getDice();
+                },
+                child: Image.asset('images/dice$leftDiceNum.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  getDice();
+                },
+                child: Image.asset('images/dice$rightDiceNum.png'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
